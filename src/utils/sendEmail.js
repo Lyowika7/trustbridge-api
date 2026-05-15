ee0c00b002e5626ce6618d8545eb31117c200c0a
LEad <lyowika@gmail.com>
2026-05-15 11:42:32 +0100

    fix smtp ipv6 connection issue

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -16,12 +16,14 @@ const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: Number(process.env.SMTP_PORT) === 465,
+    secure: false,
+    requireTLS: true,
+    family: 4,
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
-  });
+  }); 
 
   await transporter.sendMail({
     from: process.env.SMTP_FROM,
